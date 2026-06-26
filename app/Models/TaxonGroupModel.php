2884b9b409f629ac9593513f4ada6aec77494c73
John van Breda <john.vanbreda@biodiverseit.co.uk>
2026-06-26 15:14:20 +0100

    Setup GitHub actions for automated testing

--- app/Models/TaxonGroupModel.php
+++ app/Models/TaxonGroupModel.php
@@ -9,25 +9,52 @@ use CodeIgniter\Model;
  */
 class TaxonGroupModel extends Model
 {
+    /**
+     * @var string
+     */
     protected $table = 'taxon_groups';
 
+    /**
+     * @var string
+     */
     protected $primaryKey = 'id';
 
+    /**
+     * @var string
+     */
     protected $returnType = 'array';
 
+    /**
+     * @var bool
+     */
     protected $useSoftDeletes = true;
 
+    /**
+     * @var array<int, string>
+     */
     protected $allowedFields = [
         'title',
         'friendly',
         'external_key',
     ];
 
+    /**
+     * @var bool
+     */
     protected $useTimestamps = true;
 
+    /**
+     * @var string
+     */
     protected $createdField = 'created_at';
 
+    /**
+     * @var string
+     */
     protected $updatedField = 'updated_at';
 
+    /**
+     * @var string
+     */
     protected $deletedField = 'deleted_at';
 }

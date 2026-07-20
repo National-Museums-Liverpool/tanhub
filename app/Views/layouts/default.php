baefcdbfa43a4f31f377bf5d32e8267628801111
John van Breda <john.vanbreda@biodiverseit.co.uk>
2026-07-20 08:01:11 +0100

    Favicon and logo

--- app/Views/layouts/default.php
+++ app/Views/layouts/default.php
@@ -6,6 +6,7 @@
     <title><?= esc($page['pageTitle'] === $page['siteName'] ? $page['siteName'] : $page['pageTitle'] . ' | ' . $page['siteName']) ?></title>
     <meta name="description" content="<?= esc($page['metaDescription']) ?>">
     <link rel="icon" href="<?= esc(base_url('favicon.ico')) ?>" type="image/x-icon">
+    <link rel="icon" href="<?= base_url('assets/images/logo-no-text.svg') ?>" type="image/svg+xml">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700;800&family=Source+Sans+3:wght@400;600;700&display=swap" rel="stylesheet">
